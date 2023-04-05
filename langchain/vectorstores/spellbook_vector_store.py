1407c92fc15b4df59507736c8fa400edfe15b41b
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2023-04-05 16:54:40 -0700

    use spellbook client

--- langchain/vectorstores/spellbook_vector_store.py
+++ langchain/vectorstores/spellbook_vector_store.py
@@ -9,7 +9,13 @@ from langchain.embeddings.base import Embeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.base import VectorStore
 
-SPELLBOOK_ENDPOINT = 'https://canary.dashboard.scale.com/spellbook/api/v1'
+try:
+    import spellbook as sb
+except ImportError:
+    raise ValueError(
+        'Could not import redis python package. '
+        'Please install it with `pip install spellbook`.'
+    )
 
 
 class SpellbookVectorStore(VectorStore):
@@ -17,11 +23,11 @@ class SpellbookVectorStore(VectorStore):
 
     def __init__(
         self,
-        name: Optional[str] = None,
+        name: str,
         embedding_function: Optional[Embeddings] = None,
     ):
         """Initialize with Spellbook client."""
-        self._vector_store_name = name or str(uuid.uuid4())
+        self._vector_store = sb.VectorStore(name=name)
         self._embedding_function = embedding_function or OpenAIEmbeddings()
 
     def similarity_search(
@@ -31,13 +37,8 @@ class SpellbookVectorStore(VectorStore):
         **kwargs: Any,
     ) -> List[Document]:
         query_embedding = self._embedding_function.embed_query(query)
-        res = self._post_request(
-            route='similarity_search',
-            json={
-                'vectorStoreName': self._vector_store_name,
-                'queryEmbedding': query_embedding,
-                'k': k,
-            },
+        res = self._vector_store.similarity_search(
+            query_embedding=query_embedding, k=k
         )
         return [
             Document(
@@ -66,47 +67,38 @@ class SpellbookVectorStore(VectorStore):
             if i % batch_size == 32:
                 doc_ids.extend(
                     self._add_texts_batch(
+                        self._vector_store,
                         texts_batch,
                         metadatas_batch,
                         self._embedding_function,
-                        self._vector_store_name,
                     )
                 )
                 texts_batch, metadatas_batch = [], []
 
         doc_ids.extend(
             self._add_texts_batch(
+                self._vector_store,
                 texts_batch,
                 metadatas_batch,
                 self._embedding_function,
-                self._vector_store_name,
             )
         )
         return doc_ids
 
     @staticmethod
     def _add_texts_batch(
+        vector_store,
         texts: List[str],
         metadatas: List[dict],
         embedding: Embeddings,
-        name: str,
     ) -> List[str]:
         embeddings = embedding.embed_documents(texts)
-        res = SpellbookVectorStore._post_request(
-            route='upload_documents',
-            json={
-                'vectorStoreName': name,
-                'items': [
-                    {
-                        'text': text,
-                        'embedding': embedding,
-                        'metadata': metadata,
-                    }
-                    for text, embedding, metadata in zip(
-                        texts, embeddings, metadatas
-                    )
-                ],
-            },
+
+        res = vector_store.upload_documents_with_embeddings(
+            items=[
+                sb.DocumentWithEmbedding(text=t, embedding=e, metadata=m)
+                for t, e, m in zip(texts, embeddings, metadatas)
+            ]
         )
         return [doc['id'] for doc in res['data']['items']]
 
@@ -122,76 +114,31 @@ class SpellbookVectorStore(VectorStore):
     ) -> VectorStore:
         name = name or str(uuid.uuid4())
         # Create vector store, error if name exists
-        cls._post_request(
-            route='create_vector_store',
-            json={'name': name},
-        )
+        vector_store = sb.VectorStore.create(name=name)
 
         # Embed and upload documents in batches
-        ret = cls(name, embedding)
         embedding = embedding or OpenAIEmbeddings()
         for l in range(0, len(texts), batch_size):
             r = min(l + batch_size, len(texts))
             texts_batch = texts[l:r]
-            embeddings_batch = embedding.embed_documents(texts_batch)
             if metadatas:
                 metadatas_batch = metadatas[l:r]
             else:
                 metadatas_batch = [{} for _ in range(l, r)]
-            cls._post_request(
-                route='upload_documents',
-                json={
-                    'vectorStoreName': name,
-                    'items': [
-                        {
-                            'text': text,
-                            'embedding': embedding,
-                            'metadata': metadata,
-                        }
-                        for text, embedding, metadata in zip(
-                            texts_batch, embeddings_batch, metadatas_batch
-                        )
-                    ],
-                },
+            cls._add_texts_batch(
+                vector_store,
+                texts_batch,
+                metadatas_batch,
+                embedding,
             )
-        return ret
+        return cls(name=name)
 
     def delete_vector_store(self):
-        self._post_request(
-            route='delete_vector_store',
-            json={'vectorStoreName': self._vector_store_name},
-        )
+        self._vector_store.delete()
 
     def delete_documents(
         self,
         ids: List[str],
     ):
-        res = self._post_request(
-            route='delete_documents',
-            json={
-                'vectorStoreName': self._vector_store_name,
-                'ids': ids,
-            },
-        )
+        res = self._vector_store.delete_documents(ids)
         return [doc['id'] for doc in res['data']['items']]
-
-    @staticmethod
-    def _post_request(
-        route: str, json: dict, spellbook_api_key: Optional[str] = None
-    ):
-        if not spellbook_api_key:
-            spellbook_api_key = os.getenv('SPELLBOOK_API_KEY')
-            if not spellbook_api_key:
-                raise ValueError(
-                    'Could not retrieve Spellbook API key. '
-                    'Ensure your SPELLBOOK_API_KEY env variable is properly set.'
-                )
-        res = requests.post(
-            url=f'{SPELLBOOK_ENDPOINT}/{route}',
-            headers={'authorization': f'Basic {spellbook_api_key}'},
-            json=json,
-        ).json()
-        if not res['ok']:
-            # TODO: parse and format specifically for each route
-            raise ValueError(res['error'])
-        return res
